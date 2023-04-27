5d7f710d7ae014cdbfa00841bd5de25136cb852a
Robert <alex.aldea31@gmail.com>
2023-04-27 12:46:07 +0300

    Fetched the genres

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -7,7 +7,12 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="250px" borderRadius={10} overflow="hidden">
+    <Box
+      width="250px"
+      borderRadius={10}
+      overflow="hidden"
+      border="solid grey 2px"
+    >
       {children}
     </Box>
   );
