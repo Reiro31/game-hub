14950d075386c5be4a9adf07b11b8230fca0c22d
Robert <alex.aldea31@gmail.com>
2023-05-10 13:10:44 +0300

    Refactoring - added React Query

--- src/components/PlatformSelector.tsx
+++ src/components/PlatformSelector.tsx
@@ -19,7 +19,7 @@ const PlatformSelector = ({ onSelectPlatform, selectedPlatform }: Props) => {
         {selectedPlatform?.name || "Platforms"}
       </MenuButton>
       <MenuList backgroundColor="#111">
-        {data.map((platform) => (
+        {data?.results.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
             key={platform.id}
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,18 @@
+import { useQuery } from "@tanstack/react-query";
 import platforms from "../data/platforms";
+import apiClient from "../services/api-client";
+import { Platform } from "./useGames";
+import { FetchResponse } from "../services/api-client";
 
-const usePlaforms = () => ({ data: platforms, error: null });
+const usePlaforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000,
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlaforms;
